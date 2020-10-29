e5fd099a1c1b4c5ecb02db4af6c1f2a78772dd6e
Shuler Xu <shuler2097@gmail.com>
2020-10-29 16:26:00 +0000

    using a href instead of Link to

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -28,17 +28,17 @@ function Navbar() {
         <>
             <nav className='navbar'>
                 <div className='navbar-container'>
-                    <Link to='/web' className='navbar-logo' onClick = {closeMobileMenu}>
+                    <a href='/web' className='navbar-logo' onClick = {closeMobileMenu}>
                         TRVL <i className='fab fa-typo3' />
-                    </Link>
+                    </a>
                     <div className = 'menu-icon' onClick = {handleClick}>
                         <i className = {click ? 'fas fa-times' : 'fas fa-bars'} />
                     </div>
                     <ul className = {click ? 'nav-menu active' : 'nav-menu'}>
                         <li className = 'nav-item'>
-                            <Link to = '/web' className='nav-links' onClick = {closeMobileMenu}>
+                            <a href = '/web' className='nav-links' onClick = {closeMobileMenu}>
                                 Home
-                            </Link>
+                            </a>
                         </li>
                         <li className = 'nav-item'>
                             <Link to='/services' className ='nav-links' onClick = {closeMobileMenu}>
