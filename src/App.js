b365eafa3b5f7dfc2b9424076a86f5a5f1c7063a
Shuler Xu <shuler2097@gmail.com>
2020-10-29 15:26:41 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import React from 'react'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Home from './components/Pages/Home'
+import Services from './components/Pages/Services';
+import Products from './components/Pages/Products';
+import SignUp from './components/Pages/SignUp';
+
+function App() {
+  return (
+    <>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path ='/web' exact component = {Home}/>
+        <Route path ='/services' exact component = {Services}/>
+        <Route path ='/products' exact component = {Products}/>
+        <Route path ='/sign-up' exact component = {SignUp}/>
+      </Switch>
+    </Router>
+    </>
+  );
+}
+
+export default App;
